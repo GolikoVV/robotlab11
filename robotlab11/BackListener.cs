d8b00337580c0e9b77f4bb5717c122719085ef96
Лера Голикова <funtik-98@mail.ru>
2023-05-04 09:53:40 +0700

    Добавьте файлы проекта.

---
+++ robotlab11/BackListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotlab11
+{
+    // Класс, представляющий слушателя события выбора направления "назад"
+    class BackListener
+    {
+        // Метод, вызываемый при возникновении события
+        public void OnBack(object sender, EventArgs e)
+        {
+            Console.WriteLine("Робот выбрал направление назад");
+
+            // Записываем сообщение в файл
+            string text = "Робот выбрал направление назад";
+            using (StreamWriter sw = new StreamWriter("test.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ robotlab11/Robot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace RobotEvent
+{
+    // Класс, представляющий робота
+    class Robot
+    {
+        // Событие выбора направления "назад"
+        public event EventHandler Back;
+
+        // Метод, выбирающий случайное направление и вызывающий событие
+        public void ChooseDirection()
+        {
+            string[] directions = { "вперед", "назад", "влево", "вправо" };
+            Random random = new Random();
+            string direction = directions[random.Next(directions.Length)];
+
+            Console.WriteLine("Робот движется {0}", direction);
+
+            if (direction == "назад")
+            {
+                // Если выбрано направление "назад", вызываем событие
+                OnBack(EventArgs.Empty);
+            }
+        }
+
+        // Метод, вызывающий событие выбора направления "назад"
+        protected virtual void OnBack(EventArgs e)
+        {
+            EventHandler handler = Back;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
